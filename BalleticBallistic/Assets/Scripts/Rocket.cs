ddc9b495f624e16c3ef24eb5a9eb6929a7376c7f
PerryW11 <perry.winch03@gmail.com>
2020-02-06 12:15:42 -0500

    Rocket thrust audio added

---
+++ BalleticBallistic/Assets/Scripts/Rocket.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour 
+{ 
+    Rigidbody rigidBody;
+    AudioSource audio;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) // Thrust while rotating
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            if(!audio.isPlaying) // Doesn't overlap audio 
+            {
+                audio.Play();
+            }
+        }
+        else
+        {
+            audio.Stop();
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+        
+    }
+}
